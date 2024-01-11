5ece1c1acea6eb9f46f07e415341b7587f70fc9e
ViktorGakis <victor.g.gakis@gmail.com>
2024-01-11 22:04:27 +0000

    added dbquery service in Workflow

---
+++ Workflow/src/database/query_service.py
@@ -0,0 +1,8 @@
+class DatabaseQueryService:
+    def handle_query(self, *args, **kwargs):
+        """Function to query a specific instrument in the database."""
+        pass
+
+    def query_db_closure(self, *args, **kwargs):
+        """Generates a closure function for querying the database."""
+        pass

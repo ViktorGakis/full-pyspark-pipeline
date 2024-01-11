a988ca967c721d40b371647146723006e387c590
ViktorGakis <victor.g.gakis@gmail.com>
2024-01-11 15:24:48 +0200

    unified tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,14 @@ from .test_db_con import test_mysql_conx
 from .test_prepare_table import drop_table, table_preparation
 from .test_pyspark_db_con import test_pyspark_db_conx
 from .test_pyspark_inst import test_pyspark_con
+
+def run_all():
+    test_mysql_conx()
+    table_preparation()
+    test_pyspark_con()
+    test_pyspark_db_conx()
+    drop_table()
+
+
+if __name__ == "__main__":
+    run_all()

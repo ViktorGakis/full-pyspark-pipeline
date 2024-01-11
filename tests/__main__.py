c7068e2e61f30eb2549b701284b84951c003aea6
ViktorGakis <victor.g.gakis@gmail.com>
2024-01-11 13:32:38 +0000

    refactor tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -3,3 +3,13 @@ from .test_prepare_table import drop_table, table_preparation
 from .test_pyspark_db_con import test_pyspark_db_conx
 from .test_pyspark_inst import test_pyspark_con
 
+def run_all():
+    test_mysql_conx()
+    table_preparation()
+    test_pyspark_con()
+    test_pyspark_db_conx()
+    drop_table()
+
+
+if __name__ == "__main__":
+    run_all()

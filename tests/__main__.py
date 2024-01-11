8a9fc1b75c32991c103e459876e2fdada03edf16
ViktorGakis <victor.g.gakis@gmail.com>
2024-01-11 13:56:38 +0000

    improved test structure

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+# Get the parent directory of the current file (tests)
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
 from src import (
     drop_table,
     table_preparation,

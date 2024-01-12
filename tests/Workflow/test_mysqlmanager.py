5d96cc3b94c2cf70600661a77ec198c75a7b8124
ViktorGakis <victor.g.gakis@gmail.com>
2024-01-12 06:34:41 +0000

    added mysql_manager fixture

---
+++ tests/Workflow/test_mysqlmanager.py
@@ -0,0 +1,32 @@
+from unittest.mock import Mock, patch
+
+import pytest
+
+from Workflow.src import MysqlManager
+
+
+@pytest.fixture
+def config_mock() -> Mock:
+    return Mock(
+        HOST="localhost",
+        MYSQL_DOCKER_PORT=3306,
+        MYSQL_ROOT_USER="root",
+        MYSQL_ROOT_PASSWORD="password",
+        MYSQL_DATABASE="testdb",
+        TABLE_NAME="testtable",
+    )
+
+
+@pytest.fixture
+def mysql_manager(config_mock):
+    with patch("pymysql.connect") as mock_connect:
+        mock_connect.return_value = Mock()
+        manager = MysqlManager(config_mock)
+        manager.create_conx()
+        yield manager
+
+
+
+
+
+
